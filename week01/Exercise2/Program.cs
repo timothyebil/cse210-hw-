0b94ac2c315b09a7b0ce56474f7f5a8171fd1045
timothyebil <tebil@byupathway.edu>
2026-03-09 17:39:56 +0300

    Finished prep 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -3,7 +3,45 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
+    // This program prompts the user for a grade percentage, converts it to a letter grade, and indicates whether the user passed or failed.
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
