59a5a802a5cb8d6640ef42e6800b207e6919ed7a
timothyebil <tebil@byupathway.edu>
2026-03-19 06:06:55 +0300

    added content in Program.cs

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Journal journal = new Journal();
+        Entry entry = new Entry();
+        
+
         Console.WriteLine("Hello World! This is the Journal Project.");
     }
 }
