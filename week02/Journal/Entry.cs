b7f964dfbe9095e8e8d6f8718d1559faeac1be69
timothyebil <tebil@byupathway.edu>
2026-03-20 23:43:55 +0300

    Entry.cs

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -1,12 +1,28 @@
+// Entry.cs
+using System;
+
 public class Entry
 {
-    
-    public string _date;
-    public string _prompt;
-    public string _entry;
+    // Member variables (properties)
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public string Date { get; set; }
 
-    public void DisplayEntry()
+    // Constructor to easily create new entries
+    public Entry(string prompt, string response, string date)
     {
-        
+        Prompt = prompt;
+        Response = response;
+        Date = date;
+    }
+
+    // Default constructor required for JSON deserialization
+    public Entry() { }
+
+    // Method to display the entry
+    public void Display()
+    {
+        Console.WriteLine($"\nDate: {Date} - Prompt: {Prompt}");
+        Console.WriteLine($"> {Response}");
     }
 }
