cb9f91b4d49cf1271699daf85af4609c750bdf56
timothyebil <tebil@byupathway.edu>
2026-03-26 21:37:22 +0300

    Program.cs

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        // Testing constructors
+        Fraction f1 = new Fraction();
+        Console.WriteLine(f1.GetFractionString()); // Output: 1/1
+        Console.WriteLine(f1.GetDecimalValue());   // Output: 1
+
+        Fraction f2 = new Fraction(5);
+        Console.WriteLine(f2.GetFractionString()); // Output: 5/1
+        Console.WriteLine(f2.GetDecimalValue());   // Output: 5
+
+        Fraction f3 = new Fraction(3, 4);
+        Console.WriteLine(f3.GetFractionString()); // Output: 3/4
+        Console.WriteLine(f3.GetDecimalValue());   // Output: 0.75
+
+        Fraction f4 = new Fraction(1, 3);
+        Console.WriteLine(f4.GetFractionString()); // Output: 1/3
+        Console.WriteLine(f4.GetDecimalValue());   // Output: 0.3333333333333333
+
+        // Testing Setters/Getters
+        f3.SetNumerator(2);
+        f3.SetDenominator(5);
+        Console.WriteLine(f3.GetFractionString()); // Output: 2/5
+        Console.WriteLine(f3.GetDecimalValue());   // Output: 0.4
     }
 }
