d97ba86ad944a6899108741bafcda11c3137c6f4
timothyebil <tebil@byupathway.edu>
2026-03-27 03:36:41 +0300

    Scripture: Combines a Reference with a list of Word objects and manages the logic for hiding random words.

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = text.Split(' ').Select(w => new Word(w)).ToList();
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+        // Stretch: Only select words that are not already hidden
+        var visibleWords = _words.Where(w => !w.IsHidden()).ToList();
+        
+        for (int i = 0; i < numberToHide && visibleWords.Count > 0; i++)
+        {
+            int index = random.Next(visibleWords.Count);
+            visibleWords[index].Hide();
+            visibleWords.RemoveAt(index);
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string text = string.Join(" ", _words.Select(w => w.GetDisplayText()));
+        return $"{_reference.GetDisplayText()} {text}";
+    }
+
+    public bool IsCompletelyHidden() => _words.All(w => w.IsHidden());
+}
